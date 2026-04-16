f3f11b176e5be8bb107b0fe237ed5b2c2358588f
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-04-16 23:26:18 +0200

    feat: use custom async clipboard

---
+++ app/composables/useClipboardAsync.ts
@@ -0,0 +1,37 @@
+import type { ShallowRef } from 'vue'
+
+type UseClipboardAsyncReturn = {
+  copy: () => void
+  copied: ShallowRef<boolean>
+}
+
+type UseClipboardAsyncOptions = {
+  copiedDuring: number
+}
+
+export default function useClipboardAsync(
+  fn: () => Promise<string>,
+  options?: UseClipboardAsyncOptions,
+): UseClipboardAsyncReturn {
+  const copied = shallowRef(false)
+  const timeout = useTimeoutFn(() => (copied.value = false), options?.copiedDuring ?? 0, {
+    immediate: false,
+  })
+
+  function copy() {
+    const asyncClipboard = new ClipboardItem({
+      'text/plain': fn().then(text => {
+        return new Blob([text], { type: 'text/plain' })
+      }),
+    })
+
+    copied.value = true
+    navigator.clipboard.write([asyncClipboard])
+    timeout.start()
+  }
+
+  return {
+    copy,
+    copied,
+  }
+}
